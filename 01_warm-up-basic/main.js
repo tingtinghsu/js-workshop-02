14f3d96c0b221412f60ea5c663a7b33ea8bc0053
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-12 13:29:36 +0800

    workshop 1-3

--- 01_warm-up-basic/main.js
+++ 01_warm-up-basic/main.js
@@ -1,1 +1,11 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function(){
+  const myBtn = document.querySelector('button');
+  
+  myBtn.addEventListener('click',function(){
+    let circle = document.querrySelector('t#circle')
+    var clone = document.importNode(circle.content, true);
+    
+    document.querySelector('.box').appendChild(clone)
+  })
+})
--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,45 @@
-// TO DO
+document.addEventListener("DOMContentLoaded", function(){
+  // 
+  function checkedRemoved(){
+    let allList = document.querySelectorAll('li');
+    allList.forEach(function(pickedList){
+      pickedList.addEventListener('click', function(){
+        pickedList.classList.toggle("checked")
+      })
+      pickedList.querySelector('span').addEventListener('click', function(){
+        this.parentNode.remove('li')
+      })
+    })
+  }
+
+
+  let taskBtn = document.querySelector('#addBtn');
+  taskBtn.addEventListener('click', function(){
+    // 取值
+    let taskContent = document.querySelector('#input').value;
+
+    let liNew = document.createElement("li");
+    let spanNew = document.createElement("span");
+    spanNew.classList.add("close");
+    spanNew.textContent = "x";
+    let ulOrigin = document.querySelector("ul");
+
+
+    liNew.innerHTML=taskContent;
+     //  li 包 span
+    ulOrigin.appendChild(liNew).appendChild(spanNew);
+
+    let allList = document.querySelectorAll('li');
+    allList.forEach(function(pickedList){
+      pickedList.addEventListener('click', function(){
+        pickedList.classList.toggle("checked")
+      })
+      pickedList.querySelector('span').addEventListener('click', function(){
+        this.parentNode.remove('li')
+      })
+    })
+
+    // ulOrigin.lastChild(spanNew);
+    //ulOrigin.appendChild(liNew).innerHTML=taskContent;
+  })
+})
